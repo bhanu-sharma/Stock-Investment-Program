bc74edcfb892a245e83e0b613f083aa2acaef9ce
Bhanu Sharma <bhanu@spotdraft.com>
2017-11-21 14:45:34 +0530

    removed test.py

--- src/v1/test.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime
-import time
-
-path = "C:/Users/DJB/Desktop/Course Lectures/Machine Learning with Python/intraQuarter/_KeyStats"
-
-for i in os.walk(path):
-	print i
